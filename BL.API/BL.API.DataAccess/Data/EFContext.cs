c3844e703a1b45f48ecd0e453a17af02b93f938e
silverful <deadhant14@hotmail.com>
2021-11-28 17:08:00 +0300

    EF Contexts initial version + IRepository implementation

---
+++ BL.API/BL.API.DataAccess/Data/EFContext.cs
@@ -0,0 +1,19 @@
+﻿using BL.API.Core.Domain.Match;
+using BL.API.Core.Domain.Player;
+using Microsoft.EntityFrameworkCore;
+
+namespace BL.API.DataAccess.Data
+{
+    public class EFContext : DbContext
+    {
+        public EFContext(DbContextOptions<EFContext> options) : base(options) { }
+
+        public virtual DbSet<Player> Players { get; protected set; }
+        public virtual DbSet<Match> Matches { get; protected set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ BL.API/BL.API.DataAccess/Repositories/EFRepository.cs
@@ -0,0 +1,80 @@
+﻿using BL.API.Core.Abstractions.Model;
+using BL.API.Core.Abstractions.Repositories;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace BL.API.DataAccess.Repositories
+{
+    public class EFRepository<T> : IRepository<T>
+        where T : class, IBaseEntity
+    {
+        private DbContext _dbContext;
+
+        public EFRepository(DbContext context)
+        {
+            _dbContext = context;
+        }
+
+        public async Task<Guid> CreateAsync(T model)
+        {
+            await _dbContext.Set<T>().AddAsync(model);
+
+            await _dbContext.SaveChangesAsync();
+
+            return model.Id;
+        }
+
+        public async Task DeleteAsync(T model)
+        {
+            _dbContext.Set<T>().Remove(model);
+
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(Guid id)
+        {
+            var entity = await _dbContext.Set<T>().FirstOrDefaultAsync(x => x.Id == id);
+            await DeleteAsync(entity);
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await _dbContext.Set<T>().ToListAsync();
+        }
+
+        public async Task<T> GetByIdAsync(Guid id)
+        {
+            return await _dbContext.Set<T>().Where(x => x.Id == id).FirstOrDefaultAsync();
+        }
+
+        public async Task<T> GetFirstWhereAsync(Expression<Func<T, bool>> predicate)
+        {
+            return await _dbContext.Set<T>().Where(predicate).FirstOrDefaultAsync();
+        }
+
+        public async Task<IEnumerable<T>> GetRangeByIdsAsync(List<Guid> ids)
+        {
+            return await _dbContext.Set<T>().Where(x => ids.Contains(x.Id)).ToListAsync();
+        }
+
+        public async Task<IEnumerable<T>> GetWhereAsync(Expression<Func<T, bool>> predicate)
+        {
+            return await _dbContext.Set<T>().Where(predicate).ToListAsync();
+        }
+
+        public async Task SaveAsync()
+        {
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(T model)
+        {
+            _dbContext.Set<T>().Update(model);
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}

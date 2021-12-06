fa9a7a8e30a0015222dba1ccc37d06b1e13cf24c
silverful <deadhant14@hotmail.com>
2021-12-06 19:32:13 +0300

    Added additional fields

--- BL.API/BL.API.Core/Domain/Match/PlayerMatchRecord.cs
+++ BL.API/BL.API.Core/Domain/Match/PlayerMatchRecord.cs
@@ -8,6 +8,10 @@ namespace BL.API.Core.Domain.Match
         public Guid PlayerId { get; protected set; }
         [ForeignKey("PlayerId")]
         public virtual Player.Player Player { get; protected set; }
+        public Guid MatchId { get; protected set; }
+        public virtual Match Match { get; protected set; }
+        public int RoundsPlayed { get; protected set; }
+        public Faction Faction { get; protected set; }
         public int Kills { get; protected set; }
         public int Assists { get; protected set; }
         public int? Deaths { get; protected set; }

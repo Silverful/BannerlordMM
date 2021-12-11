fc0b9dde82b2536b31912d4dc4fd4cb9ce533af4
silverful <deadhant14@hotmail.com>
2021-12-12 01:23:51 +0300

    Checks and Swagger info for Player controller

--- BL.API/BL.API.WebHost/Controllers/PlayersController.cs
+++ BL.API/BL.API.WebHost/Controllers/PlayersController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 
 namespace BL.API.WebHost.Controllers
 {
@@ -23,12 +24,16 @@ namespace BL.API.WebHost.Controllers
         }
 
         [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public async Task<IActionResult> Get()
         {
             return Ok(await _mediator.Send(new GetAllPlayersQuery.Query()));
         }
 
         [HttpGet("{playerId}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public async Task<IActionResult> GetById([FromQuery] string playerId)
         {
             if (!Guid.TryParse(playerId, out Guid id)) return BadRequest();
@@ -39,6 +44,8 @@ namespace BL.API.WebHost.Controllers
         }
 
         [HttpGet("{playerId}/stats")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public async Task<IActionResult> GetPlayerStats([FromQuery] string playerId)
         {
             if (!Guid.TryParse(playerId, out Guid id)) return BadRequest();
@@ -48,7 +55,6 @@ namespace BL.API.WebHost.Controllers
             return player == null ? NotFound() : Ok(player);
         }
 
-
         [HttpGet("/stats")]
         public async Task<IActionResult> GetPlayersStats()
         {
@@ -56,21 +62,45 @@ namespace BL.API.WebHost.Controllers
         }
 
         [HttpPost]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesDefaultResponseType]
         public async Task<IActionResult> Post([FromBody] Player player)
         {
-            return Ok(await _mediator.Send(new AddPlayerCommand.Command(player)));
+            var pl = await _mediator.Send(new AddPlayerCommand.Command(player));
+            return CreatedAtAction(nameof(AddPlayerCommand), new { id = pl }, player);
         }
 
         [HttpPut]
-        public async Task<IActionResult> Put([FromBody] Player player)
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesDefaultResponseType]
+        public async Task<IActionResult> Put([FromBody] Player player, string playerId)
         {
+            if (!Guid.TryParse(playerId, out Guid id)) return BadRequest();
+
+            var dbPlayer = await _mediator.Send(new GetPlayerByIdQuery.Query(id));
+
+            if (player == null)
+                return NotFound();
+
             return Ok(await _mediator.Send(new UpdatePlayerCommand.Command(player)));
         }
 
         [HttpDelete]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesDefaultResponseType]
         public async Task<IActionResult> Delete([FromQuery] string playerId)
         {
             if (!Guid.TryParse(playerId, out Guid id)) return BadRequest();
+
+            var player = await _mediator.Send(new GetPlayerByIdQuery.Query(id));
+
+            if (player == null)
+                return NotFound();
 
             return Ok(await _mediator.Send(new DeletePlayerCommand.Command(id)));
         }

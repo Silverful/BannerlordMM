803607a05df0da91fb6a49584af4c741bb69e7ec
silverful <deadhant14@hotmail.com>
2021-12-13 13:44:04 +0300

    Added exception types

---
+++ BL.API/BL.API.Core/Exceptions/GuidCantBeParsedException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL.API.Core.Exceptions
+{
+    public class GuidCantBeParsedException : Exception
+    {
+        public GuidCantBeParsedException() : base("Guid can not be parsed")
+        {
+
+        }
+    }
+}

735f15068c80f2b435c2e9fd105cdc503291258d
silverful <deadhant14@hotmail.com>
2021-12-25 04:09:49 +0300

    ScreenshotLink index

---
+++ BL.API/BL.API.DataAccess/Migrations/20211224224235_ScreenshotLinkIndexAdded.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BL.API.DataAccess.Migrations
+{
+    public partial class ScreenshotLinkIndexAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Matches_ScreenshotLink",
+                schema: "dbo",
+                table: "Matches",
+                column: "ScreenshotLink");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Matches_ScreenshotLink",
+                schema: "dbo",
+                table: "Matches");
+        }
+    }
+}

d41b7fe87b97493f03979f37ed78beb7cc26caf3
silverful <deadhant14@hotmail.com>
2022-01-30 19:37:20 +0300

    Config commands

---
+++ BL.API/BL.API.Services/Settings/Commands/AddConfigCommand.cs
@@ -0,0 +1,41 @@
+﻿using BL.API.Core.Abstractions.Repositories;
+using BL.API.Core.Domain.Settings;
+using MediatR;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BL.API.Services.Settings.Commands
+{
+    public class AddConfigCommand : IRequest<Guid>
+    {
+        [Required]
+        public string ConfigName { get; set; }
+        public string Value { get; set; }
+
+        public class AddConfigCommandHandler : IRequestHandler<AddConfigCommand, Guid>
+        {
+            private readonly IRepository<Configuration> _repository;
+
+            public AddConfigCommandHandler(IRepository<Configuration> repository)
+            {
+                _repository = repository;
+            }
+
+
+            public async Task<Guid> Handle(AddConfigCommand request, CancellationToken cancellationToken)
+            {
+                var configuration = new Configuration
+                {
+                    ConfigName = request.ConfigName,
+                    Value = request.Value
+                };
+
+                await _repository.CreateAsync(configuration);
+
+                return configuration.Id;
+            }
+        }
+    }
+}
---
+++ BL.API/BL.API.Services/Settings/Commands/UpdateConfigCommand.cs
@@ -0,0 +1,37 @@
+﻿using BL.API.Core.Abstractions.Repositories;
+using BL.API.Core.Domain.Settings;
+using MediatR;
+using System.ComponentModel.DataAnnotations;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BL.API.Services.Settings.Commands
+{
+    public class UpdateConfigCommand : IRequest<Task>
+    {
+        [Required]
+        public string ConfigName { get; set; }
+        public string Value { get; set; }
+
+        public class UpdateConfigCommandHandler : IRequestHandler<UpdateConfigCommand, Task>
+        {
+            private readonly IRepository<Configuration> _repository;
+
+            public UpdateConfigCommandHandler(IRepository<Configuration> repository)
+            {
+                _repository = repository;
+            }
+
+            public async Task<Task> Handle(UpdateConfigCommand request, CancellationToken cancellationToken)
+            {
+                var conf = await _repository.GetFirstWhereAsync(x => x.ConfigName == request.ConfigName);
+
+                conf.Value = request.Value;
+
+                await _repository.UpdateAsync(conf);
+
+                return Task.CompletedTask;
+            }
+        }
+    }
+}

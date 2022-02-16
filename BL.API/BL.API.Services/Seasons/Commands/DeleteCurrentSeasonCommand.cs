c4cb1904531c974b6dc93eaa4df6053a4b47a462
silverful <deadhant14@hotmail.com>
2022-02-16 13:25:44 +0300

    Delete season also deletes matches and match records

--- BL.API/BL.API.Services/Seasons/Commands/DeleteCurrentSeasonCommand.cs
+++ BL.API/BL.API.Services/Seasons/Commands/DeleteCurrentSeasonCommand.cs
@@ -15,27 +15,38 @@ namespace BL.API.Services.Seasons.Commands
         {
             private readonly IRepository<Season> _seasons;
             private readonly IRepository<PlayerMMR> _mmrs;
+            private readonly IRepository<Match> _matches;
+            private readonly IRepository<PlayerMatchRecord> _records;
 
             public DeleteCurrentSeasonCommandHandler(IRepository<Season> seasons,
+                IRepository<Match> matches,
+                IRepository<PlayerMatchRecord> records,
                 IRepository<PlayerMMR> mmrs)
             {
                 _seasons = seasons;
+                _records = records;
+                _matches = matches;
                 _mmrs = mmrs;
             }
 
             public async Task<Task> Handle(DeleteCurrentSeasonCommand request, CancellationToken cancellationToken)
             {
-                var currentSeason = await _seasons.GetFirstWhereAsync(s => s.OnGoing, false);
+                var currentSeason = await _seasons.GetFirstWhereAsync(s => s.OnGoing, true);
                 var previousSeason = (await _seasons.GetWhereAsync(s => !s.OnGoing, false)).OrderByDescending(s => s.Index).First();
 
                 using (var scope = new TransactionScope(TransactionScopeAsyncFlowOption.Enabled))
                 {
-                    var currentMMRs = (await _mmrs.GetWhereAsync(m => m.SeasonId == currentSeason.Id, false)).ToList();
+                    var currentMMRs = (await _mmrs.GetWhereAsync(m => m.SeasonId == currentSeason.Id, false)).Select(x => x.Id);
 
-                    foreach (var currentMMR in currentMMRs)
-                    {
-                        await _mmrs.DeleteAsync(currentMMR);
-                    }
+                    await _mmrs.DeleteRangeAsync(currentMMRs);
+
+                    var currentRecords = (await _records.GetWhereAsync(x => x.Match.SeasonId == currentSeason.Id)).Select(x => x.Id);
+
+                    await _records.DeleteRangeAsync(currentRecords);
+
+                    var currentMatches = (await _matches.GetWhereAsync(x => x.SeasonId == currentSeason.Id)).Select(x => x.Id);
+
+                    await _matches.DeleteRangeAsync(currentMatches);
 
                     await _seasons.DeleteAsync(currentSeason.Id);
 

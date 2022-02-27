d0d06d82b8b603d4ac6e95724bbf3dfe9a073151
silverful <deadhant14@hotmail.com>
2022-02-27 23:18:35 +0300

    Configs cmds\queires use regions

--- BL.API/BL.API.Services/Settings/Commands/UpdateConfigCommand.cs
+++ BL.API/BL.API.Services/Settings/Commands/UpdateConfigCommand.cs
@@ -1,5 +1,6 @@
 ﻿using BL.API.Core.Abstractions.Repositories;
 using BL.API.Core.Domain.Settings;
+using BL.API.Services.Regions.Queries;
 using MediatR;
 using System.ComponentModel.DataAnnotations;
 using System.Threading;
@@ -12,19 +13,23 @@ namespace BL.API.Services.Settings.Commands
         [Required]
         public string ConfigName { get; set; }
         public string Value { get; set; }
+        public string RegionShortName { get; set; }
 
         public class UpdateConfigCommandHandler : IRequestHandler<UpdateConfigCommand, Task>
         {
             private readonly IRepository<Configuration> _repository;
+            private readonly IMediator _mediator;
 
-            public UpdateConfigCommandHandler(IRepository<Configuration> repository)
+            public UpdateConfigCommandHandler(IRepository<Configuration> repository, IMediator mediator)
             {
                 _repository = repository;
+                _mediator = mediator;
             }
 
             public async Task<Task> Handle(UpdateConfigCommand request, CancellationToken cancellationToken)
             {
-                var conf = await _repository.GetFirstWhereAsync(x => x.ConfigName == request.ConfigName);
+                var region = await _mediator.Send(new GetRegionByShortName.Query(request.RegionShortName));
+                var conf = await _repository.GetFirstWhereAsync(x => x.ConfigName == request.ConfigName && x.Region.ShortName == region.ShortName, false);
 
                 conf.Value = request.Value;
 
--- BL.API/BL.API.WebHost/Controllers/SettingsController.cs
+++ BL.API/BL.API.WebHost/Controllers/SettingsController.cs
@@ -22,25 +22,26 @@ namespace BL.API.WebHost.Controllers
         [HttpGet]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        public async Task<ActionResult<IEnumerable<GetAllConfigsQuery.ConfigurationResponse>>> Get()
+        public async Task<ActionResult<IEnumerable<GetAllConfigsQuery.ConfigurationResponse>>> Get(string regionShortName)
         {
-            return new JsonResult(await _mediator.Send(new GetAllConfigsQuery.Query()));
+            return new JsonResult(await _mediator.Send(new GetAllConfigsQuery.Query(regionShortName)));
         }
 
         [HttpGet("{configName}")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        public async Task<ActionResult<string>> GetConfig(string configName)
+        public async Task<ActionResult<string>> GetConfig(string regionShortName, string configName)
         {
-            return new JsonResult(await _mediator.Send(new GetConfigQuery.Query(configName)));
+            return new JsonResult(await _mediator.Send(new GetConfigQuery.Query(regionShortName, configName)));
         }
 
         [HttpPost]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status201Created)]
         [ProducesDefaultResponseType]
-        public async Task<IActionResult> Post([FromBody] AddConfigCommand request)
+        public async Task<IActionResult> Post(string regionShortName, [FromBody] AddConfigCommand request)
         {
+            request.RegionShortName = regionShortName;
             var configId = await _mediator.Send(request);
             return CreatedAtAction("Post", new { id = configId }, configId);
         }
@@ -50,8 +51,9 @@ namespace BL.API.WebHost.Controllers
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         [ProducesDefaultResponseType]
-        public async Task<IActionResult> Put(string configName, [FromBody] UpdateConfigCommand request)
+        public async Task<IActionResult> Put(string regionShortName, string configName, [FromBody] UpdateConfigCommand request)
         {
+            request.RegionShortName = regionShortName;
             if (configName != request.ConfigName) 
             {
                 return BadRequest();

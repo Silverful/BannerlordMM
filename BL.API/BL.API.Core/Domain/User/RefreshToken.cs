b8571b5c06f35157266a3d968dd3ae8736f5ab22
silverful <deadhant14@hotmail.com>
2022-03-09 00:12:18 +0300

    Refresh token adjusted

--- BL.API/BL.API.Core/Domain/User/RefreshToken.cs
+++ BL.API/BL.API.Core/Domain/User/RefreshToken.cs
@@ -1,11 +1,13 @@
-﻿using System;
+﻿using BL.API.Core.Abstractions.Model;
+using System;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace BL.API.Core.Domain.User
 {
-    public class RefreshToken
+    public class RefreshToken : IBaseEntity
     {
+        public Guid Id { get; set; }
         [Key]
         public string Token { get; set; }
         public Guid UserId { get; set; }
